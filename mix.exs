70553d613ca93ce2a2c757a346e46429c544c0db
Tanweer Shahzaad <tanweerdev@gmail.com>
2018-11-22 09:35:33 +0500

    Fix config path

--- mix.exs
+++ mix.exs
@@ -1,13 +1,24 @@
-defmodule ExSeeds.MixProject do
+defmodule FatEcto.MixProject do
   use Mix.Project
 
   def project do
     [
       app: :ex_seeds,
-      version: "0.1.0",
-      elixir: "~> 1.7",
+      version: "0.1.1",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      build_embedded: Mix.env() == :prod,
+      deps: deps(),
+      name: "ExSeeds",
+      description: description(),
+      package: package(),
+      docs: [
+        # The main page in the docs
+        main: "readme",
+        # logo: "path/to/logo.png",
+        extras: ["README.md"]
+      ],
+      source_url: "https://github.com/tanweerdev/ex_seeds"
     ]
   end
 
@@ -21,8 +32,27 @@ defmodule ExSeeds.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
+      {:csv, "~> 2.1"},
+      {:ecto, "~> 2.2"},
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
+      {:earmark, "~> 1.3", only: :dev}
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+
+  defp description() do
+    "ex_seeds provides macro to help seed records as migration"
+  end
+
+  defp package() do
+    [
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README*),
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" => "https://github.com/tanweerdev/ex_seeds",
+        "Docs" => "https://hexdocs.pm/ex_seeds/"
+      }
     ]
   end
 end
