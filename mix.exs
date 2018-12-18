481726e2674c2712d66ee494d42507a3eed4b203
Tanweer Shahzaad <tanweerdev@gmail.com>
2018-12-18 10:55:08 +0500

    Upgrade ecto to ecto 3

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule ExSeeds.MixProject do
   defp deps do
     [
       {:csv, "~> 2.1"},
-      {:ecto, "~> 2.2"},
+      {:ecto_sql, "~> 3.0"},
       {:ex_doc, "~> 0.19", only: :dev, runtime: false},
       {:earmark, "~> 1.3", only: :dev}
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
